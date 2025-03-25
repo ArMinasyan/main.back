7cd3daf5938d85ce74d3a14ec9941fabc2834abc
ArMinasyan <46679050+ArMinasyan@users.noreply.github.com>
2025-03-25 22:42:13 +0400

    IDRAM payment

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const pool = require('../config/db');
 
 exports.handleSuccess = (req, res) => {
     res.status(200).send('Payment successful!');
@@ -9,17 +10,35 @@ exports.handleFail = (req, res) => {
 };
 
 exports.handleResult = (req, res) => {
-    const { EDP_BILL_NO, EDP_REC_ACCOUNT, EDP_AMOUNT, EDP_TRANS_ID, SIGNATURE } = req.body;
+    const {
+        EDP_PRECHECK,
+        EDP_BILL_NO,
+        EDP_REC_ACCOUNT,
+        EDP_AMOUNT,
+        EDP_PAYER_ACCOUNT,
+        EDP_TRANS_ID,
+        EDP_TRANS_DATE,
+        EDP_CHECKSUM,
+      } = req.body;
+      const { IDRAM_EDP_REC_ACCOUNT, IDRAM_SECRET_KEY } = process.env;
 
-    const SECRET_KEY = process.env.SECRET_KEY;
-    const generatedSignature = crypto
-        .createHash('md5')
-        .update(`${EDP_BILL_NO}:${EDP_AMOUNT}:${SECRET_KEY}:${EDP_REC_ACCOUNT}:${EDP_TRANS_ID}`)
-        .digest('hex');
+      const [result] = pool.query('SELECT * FROM orders WHERE id = ? AND payment_type IS NULL',[EDP_BILL_NO])
+    
+      if (EDP_PRECHECK == "YES") {
+        return result?.id && EDP_REC_ACCOUNT == IDRAM_EDP_REC_ACCOUNT
+          ? res.status(200).send('OK') 
+          : res.status(200).send('');
+      } else {
+        const concatParts = `${IDRAM_EDP_REC_ACCOUNT}:${EDP_AMOUNT}:${IDRAM_SECRET_KEY}:${EDP_BILL_NO}:${EDP_PAYER_ACCOUNT}:${EDP_TRANS_ID}:${EDP_TRANS_DATE}`;
+        const checksum = crypto.createHash("md5")
+        .update(concatParts)
+        .digest("hex")
+        .toUpperCase();
+         if (EDP_CHECKSUM === checksum) {
+           pool.query('UPDATE orders SET payment_type = ? WHERE id = ?',['idram',EDP_BILL_NO ]);
+           return res.status(200).send('OK')
+        } 
 
-    if (generatedSignature === SIGNATURE) {
-        res.status(200).send('Result received and verified.');
-    } else {
-        res.status(400).send('Invalid signature.');
-    }
+       return res.status(200).send('')
+      }
 };
